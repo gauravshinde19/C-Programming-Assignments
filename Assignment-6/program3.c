891842fe04e12f1c3708b3d8ecacb73391014224
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-18 19:19:07 +0530

    Create program3.c

---
+++ Assignment-6/program3.c
@@ -0,0 +1,35 @@
+// Program to accept number from user and print factorial of that number
+
+#include <stdio.h>
+
+int Factorial(int iNo)
+{
+    int iCnt = 0;
+    int iFact = 1;
+
+    if (iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for (iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        iFact = iFact * iCnt;
+    }
+    return iFact;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d", &iValue);
+
+    iRet = Factorial(iValue);
+
+    printf("Factorial of %d :\n%d", iValue, iRet);
+
+    return 0;
+}
