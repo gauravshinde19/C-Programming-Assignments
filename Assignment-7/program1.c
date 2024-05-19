2312be4e4f97c88a110f057c1098862485677d95
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-19 19:52:00 +0530

    Create program1.c

---
+++ Assignment-7/program1.c
@@ -0,0 +1,39 @@
+/*
+    Program to accept number from user and display below pattern
+    Input - 5
+    Output - *    *    *    *    *    #    #    #    #    #
+*/
+
+#include <stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    if (iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for (iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("*\t");
+    }
+    for (iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("#\t");
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number:\n");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
