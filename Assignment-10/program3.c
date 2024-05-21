ef46641bec53e0c47316c7a950ad41e11c623ae0
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-22 04:33:11 +0530

    Create program3.c

---
+++ Assignment-10/program3.c
@@ -0,0 +1,36 @@
+// Program to accept number from user and count frequency of 2 in it
+
+#include <stdio.h>
+
+int CountTwo(int iNo)
+{
+    int iDigit = 0;
+    int iCount = 0;
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if (iDigit == 2)
+        {
+            iCount++;
+        }
+        iNo = iNo / 10;
+    }
+
+    return iCount;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the number :\n");
+    scanf("%d", &iValue);
+
+    iRet = CountTwo(iValue);
+
+    printf("Frequency of 2 in %d :\n%d", iValue, iRet);
+
+    return 0;
+}
