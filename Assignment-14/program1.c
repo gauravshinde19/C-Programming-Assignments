4caf975021c26dfeac1b77db9b607bc3a02b116c
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-05-26 11:26:09 +0530

    Create program1.c

---
+++ Assignment-14/program1.c
@@ -0,0 +1,43 @@
+/*
+    Program to accept number from user and display following pattern
+    Input - row = 4, col = 4
+    Output - A    B    C    D
+             A    B    C    D
+             A    B    C    D
+             A    B    C    D
+*/
+
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+    char cValue = '\0';
+
+    printf("Pattern :\n");
+    for (i = 1; i <= iRow; i++)
+    {
+        for (j = 1, cValue = 'A'; j <= iCol; j++, cValue++)
+        {
+            printf("%c\t", cValue);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter the number of rows :\n");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of columns :\n");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
