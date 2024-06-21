ad33c31e9fb3f84ea8a6c841633be4df41a9b0be
Gaurav Mahadev Shinde <80934597+gauravshinde19@users.noreply.github.com>
2024-06-21 23:25:55 +0530

    Create program5.c

---
+++ Assignment-32/program5.c
@@ -0,0 +1,50 @@
+/*
+    Program to accept string and display following pattern
+    Input : PPA
+    Output :
+    P
+    P  P
+    P  P  A
+*/
+#include <stdio.h>
+
+void Pattern(const char *str)
+{
+    int iLength = 0;
+    const char *iTemp = str;
+
+    while (*iTemp != '\0')
+    {
+        iLength++;
+        iTemp++;
+    }
+
+    printf("Pattern :\n");
+    for (int i = 0; i < iLength; i++)
+    {
+        for (int j = 0; j < iLength; j++)
+        {
+            if (i >= j)
+            {
+                printf("%c  ", str[j]);
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    char arr[50];
+
+    printf("Enter a string :\n");
+    scanf("%s", arr);
+
+    Pattern(arr);
+
+    return 0;
+}
